032cdb16e1864b2c0548eff7588d314da08e2298
zicongleoqu <leoqu9577@gmail.com>
2023-06-03 23:44:02 -0400

    Fixed egrok connection

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://9c96-103-106-239-104.ap.ngrok.io',
+    baseURL: 'https://f6c8-2607-9880-10e8-6c-d04f-6b20-c532-4c4c.ngrok-free.app',
     headers: {"ngrok-skip-browser-warning": "true"}
 })
