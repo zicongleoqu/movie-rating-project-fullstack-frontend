33e851716013b248c86d4aa8f1160105a126e1d9
zicongleoqu <leoqu9577@gmail.com>
2023-06-06 23:11:30 -0400

    Changed NGROK url

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://6ca8-2607-9880-10e8-6c-d04f-6b20-c532-4c4c.ngrok-free.app',
+    baseURL: 'https://2ad1-2607-9880-10e8-6c-851-317b-7e51-6fdd.ngrok-free.app',
     headers: {"ngrok-skip-browser-warning": "true"}
 })
