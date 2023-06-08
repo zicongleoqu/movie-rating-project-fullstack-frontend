22bc13b458a708af413d8ad5143f353339deadec
zicongleoqu <leoqu9577@gmail.com>
2023-06-07 22:58:30 -0400

    Tested using local host endpoint for frontend - Success

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://2ad1-2607-9880-10e8-6c-851-317b-7e51-6fdd.ngrok-free.app',
+    baseURL: 'http://localhost:8080',
     headers: {"ngrok-skip-browser-warning": "true"}
 })
